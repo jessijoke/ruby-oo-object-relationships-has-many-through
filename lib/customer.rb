b2c0c8f5128fe3721190b52d56f216a83df9bdfa
jessijoke <jessijoke@yahoo.com>
2020-11-14 00:53:05 -0500

    commit tree a3542eef304d44767e575cacdc291525570f0cbb

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,31 @@
 class Customer
-  
+    attr_accessor :name
+
+    @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+   meal = Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |m| m.customer == self }
+  end
+
+  def waiters
+    meals.collect { |meal| meal.waiter }
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
+    attr_accessor :name, :years
+    @@all = []
 
+    def initialize(name, years)
+        @name = name
+        @years = years
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip)
+        meal = Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select { |m| m.waiter == self }
+    end
+
+    def best_tipper
+        best_tipped = meals.max_by { |meal| meal.tip }
+        best_tipped.customer
+    end
 end
